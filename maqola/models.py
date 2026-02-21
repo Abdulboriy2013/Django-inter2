99f6c97de848459e2febe5bbdc1a936bcecdfab8
Abdulboriy <m31425288@gmail.com>
2026-02-21 06:02:00 +0500

    inter

--- maqola/models.py
+++ maqola/models.py
@@ -16,8 +16,8 @@ class Maqola(models.Model):
         return self.title
     
 class Comment(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    maqola = models.ForeignKey('Maqola', on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    maqola = models.ForeignKey('Maqola', on_delete=models.PROTECT)
     vaqti = models.DateTimeField(default=timezone.now)
     adminReply = models.CharField(max_length=255, null=True, blank=True)
     status = models.BooleanField(default=True)
@@ -26,8 +26,8 @@ class Comment(models.Model):
         return str(self.user)
     
 class Like(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    maqola = models.ForeignKey('Maqola', on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    maqola = models.ForeignKey('Maqola', on_delete=models.PROTECT)
     def __str__(self):
         return f'{self.user} - {self.maqola}'
     
